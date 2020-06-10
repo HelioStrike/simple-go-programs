af6491dcb338bf642262f581d68e151235f89380
HelioStrike <rageofepicfury@gmail.com>
2020-06-10 08:51:55 +0530

    hello, world! (and simple-api)

---
+++ simple-api/simple-api.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	x, err := httputil.DumpRequest(r, true)
+	log.Println(fmt.Sprintf("%q", x))
+	if err != nil {
+		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+		return
+	}
+
+	switch r.Method {
+	case "GET":
+		w.Write([]byte("Hello there!"))
+	case "POST":
+		name := r.FormValue("name")
+		w.Write([]byte(fmt.Sprintf("General, %s!", name)))
+	default:
+		w.Write([]byte("Unknown Request."))
+	}
+}
+
+func main() {
+	var port = 8080
+
+	http.HandleFunc("/", handler)
+
+	fmt.Printf("Starting server on %d...\n", port)
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
+		log.Fatal(err)
+	}
+}
